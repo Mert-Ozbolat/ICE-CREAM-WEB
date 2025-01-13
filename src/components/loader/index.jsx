4050e1d3907220842c802efad5865ae13277f65d
Mert-Ozbolat <mertozbolat008@gmail.com>
2025-01-13 13:59:52 +0300

    updated

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Loader
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  safelist: [
+    'text-[clamp(40px,7vw,100px)]',
+    'text-[clamp(18px,2vw,24px)]',
+    'text-[clamp(16px,2vw,20px)]',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
